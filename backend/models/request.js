792dde09d85b2bf4d8f2986dc911d71ce9a1e3f5
Achintya <achintya7567@gmail.com>
2023-01-28 15:46:09 +0530

    models updated

---
+++ backend/models/request.js
@@ -0,0 +1,16 @@
+const mongoose= require("mongoose");
+
+const requestSchema= new mongoose.Schema({
+    itemName: String,
+    category: String,
+    ownedBy: String,
+    requestedBy: String,
+    quantity: Number,
+    requestTime: String,
+    inTime: String,
+    outTime: String,
+    requestStatus: String,
+    remarks: String
+});
+
+module.exports(mongoose.model("Request", requestSchema));

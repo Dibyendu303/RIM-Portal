51163a9274f8788bb49932c34d55e7f465caa08e
infinity1729 <anant.bansal2003@gmail.com>
2023-02-13 02:23:15 +0530

    accept and decline api added

--- backend/controllers/requests.js
+++ backend/controllers/requests.js
@@ -1,4 +1,5 @@
 const Request = require("../models/requestSchema.js");
+const Item=require("../models/itemSchema.js")
 
 module.exports.getSentRequests= async(req, res)=>{
     try{
@@ -21,6 +22,42 @@ module.exports.getReceivedRequests= async(req, res)=>{
     }
 }
 
+module.exports.acceptRequest= async(req,res)=>{
+    try{
+        const targetRequest=await Request.findOneAndUpdate(
+            {_id:req.body.requestId},
+            {requestStatus:"Accepted"}
+        );
+        const item= await Item.findOneAndUpdate(
+            {_id:req.body.itemId},
+            {heldBy:targetRequest.requestedBy,status:"Unavailaible"}
+        );
+        res.json({
+            item: item,
+            req: targetRequest
+        })
+        
+        
+
+    }
+    catch (err){
+        res.send(err);
+        console.log(err);
+    }
+}
+
+module.exports.rejectRequest= async(req,res)=>{
+    try{
+        const targetRequest=await Request.findOneAndUpdate(
+            {_id:req.body.requestId},
+            {requestStatus:"Rejected"}
+        );
+    }
+    catch (err){
+        res.send(err);
+        console.log(err);
+    }
+}
 module.exports.newRequest = (req,res)=>{
     const request = req.body;
     const newRequest= new Request(request);
@@ -33,4 +70,3 @@ module.exports.newRequest = (req,res)=>{
         console.log(err);
     }
 }
-

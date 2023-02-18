7bb97ef95ed9782208d544f3a3f436778ac1465c
Dibyendu Dey <dibyendudey303@gmail.com>
2023-02-18 18:11:33 +0530

    Added date disabled function

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Button } from '@mui/material';
-import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import EnhancedTable from '../components/EnhancedTable';
-import Filter from '../components/filter/filter.jsx'
+import Filter from '../components/filter/Filter.jsx'
 
 
 const Main = () => {
     return (
-        <div style={{backgroundColor: "#011018", height : "1100px"}}>
+        <div style={{ backgroundColor: "#011018", height: "1100px" }}>
             <Navbar />
             <div className='h-screen flex flex-row gap-4 pt-4'>
                 <Filter></Filter>
